364457cabc0de0e1bf02b5137fb5801f479ae769
gilbertjacke <gilbertjacke@gmail.com>
2017-01-14 17:49:21 +0000

    added .gitignore

---
+++ index.js
@@ -0,0 +1,26 @@
+let wrestlers = [
+"Mark Andrews",
+"Tyler Bate",
+"Danny Burch",
+"Joseph Conners",
+"Jordan Devlin",
+"James Drake",
+"Pete Dunne",
+"HC Dyer	England",
+"Sam Gradwell",
+"Saxon Huxley",
+"Roy Johnson",
+"Dan Moloney",
+"Trent Seven",
+"Tucker	Northern",
+"Tyson T-Bone",
+"Wolfgang"
+];
+
+let individuals =
+
+ 
+// var randomIndex = Math.floor(Math.random() * textArray.length);
+// var randomElement = textArray[randomIndex];
+
+console.log(wrestlers.length)
